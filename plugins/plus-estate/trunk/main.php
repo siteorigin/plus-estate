f399fb64c69f003d3ea9c41d8665294dabea8585
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-09-09 07:01:47 +0000

    Renamed all functions to plus

--- plugins/plus-estate/trunk/main.php
+++ plugins/plus-estate/trunk/main.php
@@ -9,43 +9,43 @@ include plugin_dir_path(__FILE__).'settings.php';
 /**
  *
  */
-function estate_premium_init() {
+function estate_plus_init() {
 	if( siteorigin_setting('general_ajax_comments') ) siteorigin_ajax_comments_activate();
 	if( siteorigin_setting('layout_responsive_menu') ){
 		include plugin_dir_path(__FILE__).'mobilenav/mobilenav.php';
 	}
 }
-add_action('init', 'estate_premium_init', 11);
+add_action('init', 'estate_plus_init', 11);
 
 /**
  * @param $text
  * @return string
  */
-function estate_premium_filter_attribution($text){
+function estate_plus_filter_attribution($text){
 	if(!siteorigin_setting('general_footer_attribution')) return '';
 	else return $text;
 }
-add_filter('siteorigin_attribution_footer', 'estate_premium_filter_attribution');
+add_filter('siteorigin_attribution_footer', 'estate_plus_filter_attribution');
 
 /**
  * Add the responsive layout
  */
-function estate_premium_responsive_header(){
+function estate_plus_responsive_header(){
 	if( siteorigin_setting('layout_responsive') ) {
 		?><meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" /><?php
 	}
 }
-add_action('wp_head', 'estate_premium_responsive_header');
+add_action('wp_head', 'estate_plus_responsive_header');
 
 /**
  * Enqueue the CSS required for the responsive layout
  */
-function estate_premium_responsive_css(){
+function estate_plus_responsive_css(){
 	if(siteorigin_setting('layout_responsive')) {
 		wp_enqueue_style('estate-responsive', plugin_dir_url(__FILE__).'assets/responsive.css', array(), SITEORIGIN_THEME_VERSION);
 	}
 }
-add_action('wp_enqueue_scripts', 'estate_premium_responsive_css', 11);
+add_action('wp_enqueue_scripts', 'estate_plus_responsive_css', 11);
 
 /**
  * Add custom body classes.
@@ -55,13 +55,13 @@ add_action('wp_enqueue_scripts', 'estate_premium_responsive_css', 11);
  * @package clearly
  * @since 1.0
  */
-function estate_premium_body_class($classes){
+function estate_plus_body_class($classes){
 	if(siteorigin_setting('layout_responsive')) $classes[] = 'responsive';
 	return $classes;
 }
-add_filter('body_class', 'estate_premium_body_class');
+add_filter('body_class', 'estate_plus_body_class');
 
-function estate_premium_mobilenav_res(){
+function estate_plus_mobilenav_res(){
 	return 720;
 }
-add_filter('siteorigin_mobilenav_resolution', 'estate_premium_mobilenav_res');
+add_filter('siteorigin_mobilenav_resolution', 'estate_plus_mobilenav_res');
--- plugins/plus-estate/trunk/settings.php
+++ plugins/plus-estate/trunk/settings.php
@@ -3,11 +3,11 @@
 /**
  * Add all the premium settings.
  */
-function estate_premium_settings_init(){
+function estate_plus_settings_init(){
 	siteorigin_settings_add_field('general', 'footer_attribution', 'checkbox');
 	siteorigin_settings_add_field('general', 'logo_retina', 'media');
 	siteorigin_settings_add_field('general', 'ajax_comments', 'checkbox');
 	siteorigin_settings_add_field('layout', 'responsive', 'checkbox');
 	siteorigin_settings_add_field('layout', 'responsive_menu', 'checkbox');
 }
-add_action('admin_init', 'estate_premium_settings_init');
+add_action('admin_init', 'estate_plus_settings_init');
